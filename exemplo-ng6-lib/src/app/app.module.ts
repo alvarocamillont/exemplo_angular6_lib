07cd59738a69a0fc1c5a957d24263e20713dff23
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2018-08-13 11:19:32 -0300

    Utilizando a biblioteca

--- exemplo-ng6-lib/src/app/app.module.ts
+++ exemplo-ng6-lib/src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ExemploNg6LibModule } from "exemplo-ng6-lib";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ExemploNg6LibModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
