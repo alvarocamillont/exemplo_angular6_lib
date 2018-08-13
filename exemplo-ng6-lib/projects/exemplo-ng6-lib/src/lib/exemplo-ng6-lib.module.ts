f3e16a0848c38db673b8310e5b24b8846107e68e
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2018-08-13 11:30:35 -0300

    Criando novo componente na biblioteca

--- exemplo-ng6-lib/projects/exemplo-ng6-lib/src/lib/exemplo-ng6-lib.module.ts
+++ exemplo-ng6-lib/projects/exemplo-ng6-lib/src/lib/exemplo-ng6-lib.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { ExemploNg6LibComponent } from './exemplo-ng6-lib.component';
+import { NgModule } from "@angular/core";
+import { ExemploNg6LibComponent } from "./exemplo-ng6-lib.component";
+import { FooComponent } from "./foo/foo.component";
 
 @NgModule({
-  imports: [
-  ],
-  declarations: [ExemploNg6LibComponent],
-  exports: [ExemploNg6LibComponent]
+  imports: [],
+  declarations: [ExemploNg6LibComponent, FooComponent],
+  exports: [ExemploNg6LibComponent, FooComponent]
 })
-export class ExemploNg6LibModule { }
+export class ExemploNg6LibModule {}
--- exemplo-ng6-lib/projects/exemplo-ng6-lib/src/public_api.ts
+++ exemplo-ng6-lib/projects/exemplo-ng6-lib/src/public_api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of exemplo-ng6-lib
  */
 
-export * from './lib/exemplo-ng6-lib.service';
-export * from './lib/exemplo-ng6-lib.component';
-export * from './lib/exemplo-ng6-lib.module';
+export * from "./lib/exemplo-ng6-lib.service";
+export * from "./lib/exemplo-ng6-lib.component";
+export * from "./lib/exemplo-ng6-lib.module";
+export * from "./lib/foo/foo.component";
